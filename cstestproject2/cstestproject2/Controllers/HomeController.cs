6d680072fc3bd35692c63922d6171e6e6e099444
Eunsaem Lee <eunsaemy@gmail.com>
2021-04-08 01:18:46 -0700

    Booking Appointment pages

--- cstestproject2/cstestproject2/Controllers/HomeController.cs
+++ cstestproject2/cstestproject2/Controllers/HomeController.cs
@@ -72,6 +72,11 @@ namespace cstestproject2.Controllers
             return View();
         }
 
+        public IActionResult BookingAppointment()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
